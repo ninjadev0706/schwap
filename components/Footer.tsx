05cc08a1e675ce63d7e486aaee96ee302b0bb0ff
ninjadev0706 <ninjadeveloper0706@gmail.com>
2023-04-07 04:42:53 +0900

    changed icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,37 +4,37 @@ export default function Footer() {
     return <div className='flex justify-center mb-20'>
       <div className='flex gap-2 sm:gap-4'>
         <div className="box_shadow">
-          <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
+          <div className='rounded-xl p-2 sm:p-4 bg-gray-200 relative z-10 '>
             <a href="http://twitter.com/0xSchwap" rel="noreferrer" target="_blank">
-              <Icon icon="mdi:twitter" width="30" color="gray" />
+              <Icon icon="mdi:twitter" width="25" color="gray" />
             </a>
           </div>
         </div>
         <div className="box_shadow">
-          <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
+          <div className='rounded-xl p-2 sm:p-4 bg-gray-200 relative z-10'>
             <a href="http://twitter.com/0xSchwap" rel="noreferrer" target="_blank">
-              <Icon icon="ic:baseline-discord" width="30" color="gray" />
+              <Icon icon="ic:baseline-discord" width="25" color="gray" />
             </a>
           </div>
         </div>
         <div className="box_shadow">
-          <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
+          <div className='rounded-xl p-2 sm:p-4 bg-gray-200 relative z-10'>
             <a href="http://twitter.com/0xSchwap" rel="noreferrer" target="_blank">
-              <Icon icon="simple-icons:substack" width="30" color="gray" />
+              <Icon icon="simple-icons:substack" width="25" color="gray" />
             </a>
           </div>
         </div>
         <div className="box_shadow">
-          <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
+          <div className='rounded-xl p-2 sm:p-4 bg-gray-200 relative z-10'>
             <a href="http://twitter.com/0xSchwap" rel="noreferrer" target="_blank">
-              <Icon icon="simple-icons:gitbook" width="30" color="gray" />
+              <Icon icon="simple-icons:gitbook" width="25" color="gray" />
             </a>
           </div>
         </div>
         <div className="box_shadow">
-          <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
+          <div className='rounded-xl p-2 sm:p-4 bg-gray-200 relative z-10'>
             <a href="https://github.com/0xschwap" rel="noreferrer" target="_blank">
-              <Icon icon="mdi:github" width="30" color="gray" />
+              <Icon icon="mdi:github" width="25" color="gray" />
             </a>
           </div>
         </div>
