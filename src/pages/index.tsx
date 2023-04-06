2c5290861589b0d74233064cb3c5976bb4da20dc
ninjadev0706 <ninjadeveloper0706@gmail.com>
2023-04-06 16:00:50 +0900

    changed logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
 import styles from 'styles/Home.module.scss'
 import { Icon } from '@iconify/react'
-import { ThemeToggleButton, ThemeToggleList } from 'components/Theme'
-import { useState } from 'react'
-import { useNetwork, useSwitchNetwork, useAccount, useBalance, useConnect, useDisconnect } from 'wagmi'
-import ConnectWallet from 'components/Connect/ConnectWallet'
 import MainBox from 'components/MainBox'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { useConnectModal, useAccountModal, useChainModal } from '@rainbow-me/rainbowkit'
-import { Chain, configureChains } from 'wagmi'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 export default function Home() {
   return (
     <div className="bg-gray-200 h-screen flex flex-col justify-center pt-20">
       <Header />
-      {/* <Main /> */}
       <MainBox />
       <Footer />
     </div>
@@ -32,47 +23,6 @@ function Header() {
         </div>
       </div>
     </header>
-  )
-}
-
-function Main() {
-  // const { address, isConnected, connector } = useAccount()
-  // const { chain, chains } = useNetwork()
-  // const { isLoading: isNetworkLoading, pendingChainId, switchNetwork } = useSwitchNetwork()
-  // const { data: balance, isLoading: isBalanceLoading } = useBalance({
-  //   address: address,
-  // })
-  // const { openConnectModal } = useConnectModal()
-  // const { openAccountModal } = useAccountModal()
-  // const { openChainModal } = useChainModal()
-  return (
-    <main className={styles.main + ' space-y-6'}>
-      <div className="text-center"></div>
-
-      {/* <div>
-        <div className="flex w-full flex-col items-center">
-          <ConnectWallet />
-        </div>
-      </div>
-
-      <div>
-        <div className="flex w-full flex-col items-center">
-          {openConnectModal && (
-            <button
-              onClick={openConnectModal}
-              type="button"
-              className="m-1 hidden rounded-lg bg-orange-500 px-3 py-1 text-white transition-all duration-150 hover:scale-105"
-            >
-              useConnectModal
-            </button>
-          )}
-        </div>
-      </div> */}
-
-      <div>
-        <div className="flex w-full flex-col items-center"></div>
-      </div>
-    </main>
   )
 }
 
