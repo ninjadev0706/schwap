a4bbe7915796078b8983735b0244b7726f59daee
ninjadev0706 <ninjadeveloper0706@gmail.com>
2023-04-07 00:37:06 +0900

    changed wallet config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,16 @@ import { app } from 'appConfig'
 import { useState, useEffect } from 'react'
 import HeadGlobal from 'components/HeadGlobal'
 // Web3Wrapper deps:
-import { getDefaultWallets, RainbowKitProvider, lightTheme, darkTheme } from '@rainbow-me/rainbowkit'
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { connectorsForWallets, getDefaultWallets, RainbowKitProvider, lightTheme, darkTheme } from '@rainbow-me/rainbowkit'
+import {
+  injectedWallet,
+  metaMaskWallet,
+  braveWallet,
+  coinbaseWallet,
+  walletConnectWallet,
+  ledgerWallet,
+  rainbowWallet,
+} from '@rainbow-me/rainbowkit/wallets'
 import { Chain } from '@rainbow-me/rainbowkit'
 import { arbitrum } from 'wagmi/chains'
 import { createClient, configureChains, WagmiConfig } from 'wagmi'
@@ -25,35 +30,32 @@ const { chains, provider } = configureChains(
   [publicProvider()]
 )
 
-const { connectors } = getDefaultWallets({
-  appName: "schwap",
-  chains,
-});
+const otherWallets = [
+  braveWallet({ chains }),
+  ledgerWallet({ chains }),
+  coinbaseWallet({ chains, appName: app.name }),
+  rainbowWallet({ chains }),
+]
+
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Recommended',
+    wallets: [injectedWallet({ chains }), metaMaskWallet({ chains }), walletConnectWallet({ chains })],
+  },
+  {
+    groupName: 'Other Wallets',
+    wallets: otherWallets,
+  },
+])
+
+// const { connectors } = getDefaultWallets({
+//   appName: "schwap",
+//   chains,
+// });
 
 const wagmiClient = createClient({
   autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'wagmi',
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        projectId: '359fc5e059fbd15f68fff8a3f391894c',
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: {
-        name: 'Injected',
-        shimDisconnect: true,
-      },
-    }),
-  ],
+  connectors,
   provider
 });
 
