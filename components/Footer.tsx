5a2b6cda7edcc1d6085095ee5f7adf919e8d2eef
ninjadev0706 <ninjadeveloper0706@gmail.com>
2023-04-07 02:35:25 +0900

    added links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,35 +5,35 @@ export default function Footer() {
       <div className='flex gap-2 sm:gap-4'>
         <div className="box_shadow">
           <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
-            <a href="https://schwap.app">
+            <a href="http://twitter.com/0xSchwap" rel="noreferrer" target="_blank">
               <Icon icon="mdi:twitter" width="30" color="gray" />
             </a>
           </div>
         </div>
         <div className="box_shadow">
           <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
-            <a href="https://schwap.app">
+            <a href="http://twitter.com/0xSchwap" rel="noreferrer" target="_blank">
               <Icon icon="ic:baseline-discord" width="30" color="gray" />
             </a>
           </div>
         </div>
         <div className="box_shadow">
           <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
-            <a href="https://schwap.app">
+            <a href="http://twitter.com/0xSchwap" rel="noreferrer" target="_blank">
               <Icon icon="simple-icons:substack" width="30" color="gray" />
             </a>
           </div>
         </div>
         <div className="box_shadow">
           <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
-            <a href="https://schwap.app">
+            <a href="http://twitter.com/0xSchwap" rel="noreferrer" target="_blank">
               <Icon icon="simple-icons:gitbook" width="30" color="gray" />
             </a>
           </div>
         </div>
         <div className="box_shadow">
           <div className='rounded-xl p-4 bg-gray-200 relative z-10'>
-            <a href="https://schwap.app">
+            <a href="https://github.com/0xschwap" rel="noreferrer" target="_blank">
               <Icon icon="mdi:github" width="30" color="gray" />
             </a>
           </div>
